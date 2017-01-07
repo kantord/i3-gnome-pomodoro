39b10ff55a9bf6157eaf3c40245c07039708bb5b
Dániel Kántor <git@daniel-kantor.com>
2017-01-07 21:45:01 +0100

    Initial implementation of dbus client

---
+++ pomodoro-client.py
@@ -0,0 +1,56 @@
+import math
+from pydbus import SessionBus
+
+
+def get_pomodoro_proxy():
+    return SessionBus().get("org.gnome.Pomodoro", "/org/gnome/Pomodoro")
+
+
+def format_time(seconds):
+    return "{minutes}:{seconds}".format(
+        minutes=math.floor(seconds / 60),
+        seconds=math.floor(seconds % 60)
+    )
+
+
+def format_is_paused(is_paused):
+    return " PAUSED" if is_paused else ""
+
+
+def format_state(state):
+    return {
+        "pomodoro": "Pomodoro",
+        "break": "Break",
+    }[state]
+
+
+def extract_pomodoro_data(pomodoro):
+    return {
+        "elapsed": pomodoro.Elapsed, "is_paused": pomodoro.IsPaused,
+        "duration": pomodoro.StateDuration,
+        "remaining": pomodoro.StateDuration - pomodoro.Elapsed,
+        "state": pomodoro.State
+    }
+
+def format_pomodoro_data(pomodoro_data):
+    return {
+        "elapsed": format_time(pomodoro_data["elapsed"]),
+        "duration": format_time(pomodoro_data["duration"]),
+        "remaining": format_time(pomodoro_data["remaining"]),
+        "is_paused": format_is_paused(pomodoro_data["is_paused"]),
+        "state": format_state(pomodoro_data["state"]),
+    }
+
+def format_output(formatted_pomodoro_data):
+    return "{state} {remaining} {is_paused}".format(**formatted_pomodoro_data)
+
+
+def main():
+    pomodoro = get_pomodoro_proxy()
+    pomodoro_data = extract_pomodoro_data(pomodoro)
+    formatted_pomodoro_data = format_pomodoro_data(pomodoro_data)
+    print(format_output(formatted_pomodoro_data))
+
+
+if __name__ == "__main__":
+    main()
